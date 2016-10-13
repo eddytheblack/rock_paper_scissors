0c36baae6017a962d6d7b7a32091b7cb126e10f3
Edwin Kamulegeya <edwinkamulegeya@Edwins-MBP.default>
2016-10-13 13:32:54 +0100

    Changed the name of the view appropriately

--- app/gameView/rock_paper_scissors.js
+++ app/gameView/rock_paper_scissors.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.gameView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/rock_paper_scissors.html',
+  $routeProvider.when('/gameView', {
+    templateUrl: 'gameView/rock_paper_scissors.html',
     controller: 'RockPaperScissorsCtrl'
   });
 }])
--- app/gameView/rock_paper_scissors_test.js
+++ app/gameView/rock_paper_scissors_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.gameView module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.gameView'));
   
   var $controller;
 
